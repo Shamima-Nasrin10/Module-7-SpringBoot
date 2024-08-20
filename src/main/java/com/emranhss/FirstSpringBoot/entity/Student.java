5643232f89df4c16163024b3098153a2bcd6918b
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-20 15:08:10 +0600

    third commit

--- src/main/java/com/emranhss/FirstSpringBoot/entity/Student.java
+++ src/main/java/com/emranhss/FirstSpringBoot/entity/Student.java
@@ -22,4 +22,76 @@ public class Student {
 
     private Date dob;
 
+    public Student() {
+    }
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public Student(String name, String email, String cell, String gender, Date dob) {
+        this.name = name;
+        this.email = email;
+        this.cell = cell;
+        this.gender = gender;
+        this.dob = dob;
+    }
+
+    public Student(int id, String email, String name, String cell, String gender, Date dob) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.cell = cell;
+        this.gender = gender;
+        this.dob = dob;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCell() {
+        return cell;
+    }
+
+    public void setCell(String cell) {
+        this.cell = cell;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
