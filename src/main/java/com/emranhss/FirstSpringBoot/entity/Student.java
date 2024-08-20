35a5bed37185270c29d10f899c7b7c4fb266f34b
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-20 14:46:59 +0600

    First Commit

---
+++ src/main/java/com/emranhss/FirstSpringBoot/entity/Student.java
@@ -0,0 +1,25 @@
+package com.emranhss.FirstSpringBoot.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+    @Column(nullable = false, length = 40, name = "studentName")
+    private String name;
+    @Column(nullable = false, unique = true)
+    private  String email;
+
+    @Column(nullable = false, unique = true)
+    private  String cell;
+    private  String gender;
+
+    private Date dob;
+
+}
