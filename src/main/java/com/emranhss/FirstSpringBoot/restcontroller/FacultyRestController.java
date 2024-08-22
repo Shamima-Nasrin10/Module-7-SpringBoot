4ae9f56d562dfe9b49e1ba58fc6253cbd9fcc178
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-22 18:56:49 +0600

    update commit

---
+++ src/main/java/com/emranhss/FirstSpringBoot/restcontroller/FacultyRestController.java
@@ -0,0 +1,37 @@
+package com.emranhss.FirstSpringBoot.restcontroller;
+
+import com.emranhss.FirstSpringBoot.entity.Faculty;
+import com.emranhss.FirstSpringBoot.service.FacultyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/faculty")
+public class FacultyRestController {
+
+    @Autowired
+    private FacultyService facultyService;
+
+    @GetMapping("/list")
+    public List<Faculty> getAllFaculty() {
+        return facultyService.getAllFaculty();
+    }
+
+
+    @PostMapping("/save")
+    public void saveFaculty(@RequestBody Faculty f) {
+        facultyService.saveFaculty(f);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteFaculty(@PathVariable("id") int id) {
+        facultyService.deleteFacultyById(id);
+    }
+
+    @PutMapping("/update")
+    public void updateFaculty(@RequestBody Faculty f) {
+        facultyService.updateFaculty(f);
+    }
+}
