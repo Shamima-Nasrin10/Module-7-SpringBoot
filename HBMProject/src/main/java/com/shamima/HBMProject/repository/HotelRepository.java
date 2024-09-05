caf4a18aa99db1e0e84c0ce9d105797d82ec6ad0
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-09-05 16:28:10 +0600

    hotel Repository

--- HBMProject/src/main/java/com/shamima/HBMProject/repository/HotelRepository.java
+++ HBMProject/src/main/java/com/shamima/HBMProject/repository/HotelRepository.java
@@ -3,5 +3,8 @@ package com.shamima.HBMProject.repository;
 import com.shamima.HBMProject.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+    List<Hotel> findHotelsByLocationName(String locationName);
 }
