09ec7407066df37c83797c5966db50d4b6947ff4
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-09-10 15:16:47 +0600

    auth
    Authentication process of my supply chain management software has been started.

---
+++ SCMSystem/src/main/java/com/shamima/SCMSystem/entity/Role.java
@@ -0,0 +1,8 @@
+package com.shamima.SCMSystem.entity;
+
+public enum Role {
+
+    ADMIN,
+    USER,
+    Customer
+}
--- SCMSystem/src/main/java/com/shamima/SCMSystem/entity/User.java
+++ SCMSystem/src/main/java/com/shamima/SCMSystem/entity/User.java
@@ -1,10 +1,10 @@
 package com.shamima.SCMSystem.entity;
 
-import jakarta.persistence.Access;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
+import java.sql.Date;
 
 @Entity
 @NoArgsConstructor
@@ -12,9 +12,60 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    private int id;
-    private String username;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
-    private String role;
+
+    @Column(unique = true)
+    private String cell;
+    private String address;
+    private Date dob;
+    private String gender;
+    private String image;
+
+//    @Enumerated(value = EnumType.STRING)
+//    private Role role;
+//
+//    @OneToMany(mappedBy = "user")
+//    private List<Token> tokens;
+//
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of(new SimpleGrantedAuthority(role.name()));
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return email;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
 }
