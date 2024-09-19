970db2338b383a5795712adbb9dfd0f92cc4537f
Shamima-Nasrin10 <shamima48nasrin@gmail.com>
2024-09-20 00:48:00 +0600

    permit all

--- SCMSystem/src/main/java/com/shamima/SCMSystem/security/SecurityConfig.java
+++ SCMSystem/src/main/java/com/shamima/SCMSystem/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                         .authorizeHttpRequests(
 
                                 req ->
-                                        req.requestMatchers("/login", "/register")
+                                        req.requestMatchers("/login", "/register", "api/rawmaterial", "api/supplier")
                                                 .permitAll()
 //                                                .requestMatchers("api/hotel/save", "api/room/save")
 //                                                .hasAuthority("ADMIN")
