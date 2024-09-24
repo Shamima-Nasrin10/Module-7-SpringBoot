6eaf48d5edafebdd61e2b4983a7c09d286648ec8
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-09-24 19:00:59 +0600

    inventory controller

---
+++ SCMSystem/src/main/java/com/shamima/SCMSystem/goods/restcontroller/InventoryController.java
@@ -0,0 +1,42 @@
+package com.shamima.SCMSystem.goods.restcontroller;
+
+
+import com.shamima.SCMSystem.goods.entity.Inventory;
+import com.shamima.SCMSystem.goods.service.InventoryService;
+import com.shamima.SCMSystem.util.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+    @Autowired
+    private InventoryService inventoryService;
+    
+    @PostMapping("/save")
+    public ApiResponse saveInventory(@RequestPart Inventory inventory,
+                                     @RequestParam Long warehouseId) {
+        return inventoryService.saveInventory(inventory, warehouseId);
+    }
+
+    @GetMapping("/list")
+    public ApiResponse getInventories() {
+        return inventoryService.getAllInventories();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse getInventoryById(@PathVariable long id) {
+        return inventoryService.findInventoryById(id);
+    }
+
+    @PutMapping("/update")
+    public ApiResponse updateInventory(@RequestPart Inventory inventory,
+                                       @RequestParam Long warehouseId) {
+        return inventoryService.updateInventory(inventory, warehouseId);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ApiResponse deleteInventoryById(@PathVariable long id) {
+        return inventoryService.deleteInventoryById(id);
+    }
+}
