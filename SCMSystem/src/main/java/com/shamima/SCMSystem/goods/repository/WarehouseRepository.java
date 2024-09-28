b9e3c5cfb7f01df20377383a8bf4cdb787c816da
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-09-28 18:39:25 +0600

    country

--- SCMSystem/src/main/java/com/shamima/SCMSystem/goods/repository/WarehouseRepository.java
+++ SCMSystem/src/main/java/com/shamima/SCMSystem/goods/repository/WarehouseRepository.java
@@ -3,10 +3,10 @@ package com.shamima.SCMSystem.goods.repository;
 import com.shamima.SCMSystem.goods.entity.Inventory;
 import com.shamima.SCMSystem.goods.entity.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
-
     List<Inventory> findInventoryById(Long id);
 }
