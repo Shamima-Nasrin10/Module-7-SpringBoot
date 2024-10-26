8f0742c4fff7020832ed7b1b82fde786ad421c69
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-10-26 17:22:16 +0600

    Role

--- SCMSystem/src/main/java/com/shamima/SCMSystem/security/restcontroller/AuthenticationController.java
+++ SCMSystem/src/main/java/com/shamima/SCMSystem/security/restcontroller/AuthenticationController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api")
-@CrossOrigin("*")
 public class AuthenticationController {
 
 private final AuthService authService;
